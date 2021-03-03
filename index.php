0753635dbcabe1f90396e33af477a998ba7b7ccd
novanilhamr <novanadcrhdhr@gmail.com>
2021-03-03 09:55:26 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
     
     <h1>hello ginan</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque non quasi dicta eveniet ad. Laboriosam sapiente nam, explicabo praesentium dolore nisi totam eveniet nulla aperiam atque sunt in maiores repellat.</p>
 
 </body>
 </html>
